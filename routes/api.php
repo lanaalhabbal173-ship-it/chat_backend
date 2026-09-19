6b3164ded207cfa1c94b3a055d7a6b4ee09acad1
lanaalhabbal <lanaalhabbal173@gmail.com>
2026-09-19 15:09:00 +0300

    Add users list route

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,14 @@
 
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+
+
+Route::middleware('auth:sanctum')->get('/users', function (Request $request) {
+    return User::where('id', '!=', $request->user()->id)->get();
+});
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
