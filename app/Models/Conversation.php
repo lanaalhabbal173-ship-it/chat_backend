7b0f91168f76348cfca47ae93a8f8740a6adea70
lanaalhabbal <lanaalhabbal173@gmail.com>
2026-09-20 18:42:10 +0300

    Add latest message relation

--- app/Models/Conversation.php
+++ app/Models/Conversation.php
@@ -3,6 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Conversation extends Model
 {
@@ -11,18 +14,33 @@ class Conversation extends Model
         'user_two_id',
     ];
 
-    public function userOne()
+    public function userOne(): BelongsTo
     {
-        return $this->belongsTo(User::class, 'user_one_id');
+        return $this->belongsTo(
+            User::class,
+            'user_one_id'
+        );
     }
 
-    public function userTwo()
+    public function userTwo(): BelongsTo
     {
-        return $this->belongsTo(User::class, 'user_two_id');
+        return $this->belongsTo(
+            User::class,
+            'user_two_id'
+        );
     }
 
-    public function messages()
+    public function messages(): HasMany
     {
-        return $this->hasMany(Message::class);
+        return $this->hasMany(
+            Message::class
+        );
+    }
+
+    public function latestMessage(): HasOne
+    {
+        return $this
+            ->hasOne(Message::class)
+            ->latestOfMany();
     }
 }
